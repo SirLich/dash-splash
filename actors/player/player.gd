a0a0c23896f9dfa5c3fc47128987f0bbab149c8d
SirLich <lrkoehler77@gmail.com>
2025-01-24 22:31:37 +0100

    Add basic movement

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+@export var speed = 200
+@export var turn_speed = 5.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Rotation
+	var desired_rotation = global_position.angle_to_point(get_global_mouse_position()) + PI/2.0
+	global_rotation = rotate_toward(global_rotation, desired_rotation, delta * turn_speed)
+	
+	# Movement
+	global_position += global_transform.y * speed * delta
