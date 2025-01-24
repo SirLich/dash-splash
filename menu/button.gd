fdbc7ed01a14ae6915c80487be327e5108188176
PhamPhiL <phi-long.pham@infineon.com>
2025-01-24 23:14:56 +0100

    fixed main menu and start bubble_spawner

--- menu/button.gd
+++ menu/button.gd
@@ -1,11 +1,10 @@
 extends Button
 
+@export
+var gamescene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	pressed.connect(self.button_pressed)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func button_pressed():
+	get_tree().change_scene_to_packed(gamescene)
