2c57f835ec3077564221a5af91612c1cbb341d36
SirLich <lrkoehler77@gmail.com>
2025-01-25 11:18:33 +0100

    Add initial movement code

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -3,20 +3,51 @@ extends Node2D
 @export var speed_increase = 200
 @export var turn_speed = 5.0
 @export var max_velocity = 200
-@export var gravity = 9.8
+@export var gravity = 450.8
 
-@export var base_speed = 20
-@export var acceleration = 20
+@export var speed = 150
+@export var acceleration = 500
+@export var rotation_speed = 20.0
+@export var friction = 900
+@export var min_speed = 100
+@export var max_speed = 2000
+@export var air_velocity = Vector2()
 
 var is_in_bubble = true
 var velocity = Vector2()
 
+	
 func _process(delta):
 	slither_movement(delta)
+
+func is_boosting():
+	return Input.is_action_pressed("boost")
 	
 func slither_movement(delta):
-	pass
-	
+	if is_in_bubble:
+		# Rotation
+		var desired_rotation = global_position.angle_to_point(get_global_mouse_position()) + PI/2.0
+		global_rotation = rotate_toward(global_rotation, desired_rotation, delta * rotation_speed)
+		
+		# Apply Friction and Acceleration
+		if is_boosting():
+			speed += acceleration * delta
+		else:
+			speed -= friction * delta
+			
+		speed = clamp(speed, min_speed, max_speed)
+		
+		# Update Transform
+		air_velocity = -global_transform.y * speed
+		global_position += -global_transform.y * speed * delta
+		
+		$Label.text = str(speed)
+	else:
+		global_rotation = air_velocity.normalized().angle() + 90
+		air_velocity.y += gravity * delta
+		global_position += air_velocity * delta
+
+
 func velocity_movement(delta):
 	if is_in_bubble:
 		# Rotation
@@ -34,7 +65,7 @@ func velocity_movement(delta):
 	global_position += velocity * delta
 	
 	
-	$Label.text = str(velocity)	
+	$Label.text = str(velocity)
 
 
 func _on_area_2d_area_entered(area):
