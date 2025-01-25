5a2a85b2720ab5796c242acc9d5eaf7736b83ca5
SirLich <lrkoehler77@gmail.com>
2025-01-25 11:29:36 +0100

    Import assets and add bubble scaling

--- actors/bubble/bubble.gd
+++ actors/bubble/bubble.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+@export var initial_size = 1.0
+
+var default_scale_sprite : float
+var default_scale_shape : float
+
+@export var sprite : Sprite2D
+@export var shape : CollisionShape2D
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	default_scale_sprite = sprite.scale.x
+	default_scale_shape = shape.shape.radius
+	set_bubble_scale(initial_size)
+	
+# 1.0 is normal, 2.0 is double, etc.
+func set_bubble_scale(size):
+	var new_sprite_scale = default_scale_sprite * size
+	sprite.scale = Vector2(new_sprite_scale, new_sprite_scale)
+	
+	shape.shape.radius = default_scale_shape * size
+	
+	
+	
