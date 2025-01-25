d5fba331bdc6c140fb9f0ec58d572d0470e5b7ba
SirLich <lrkoehler77@gmail.com>
2025-01-25 11:59:50 +0100

    Add body follow1

---
+++ actors/body/body.gd
@@ -0,0 +1,19 @@
+extends Node2D
+class_name Body
+
+var snap_duration = 0.2
+var following : Node2D
+var follow_distance = 100
+
+var tween : Tween
+
+func set_following(node : Node2D, snap: float, follow: float):
+	following = node
+	snap_duration = snap
+	follow_distance = follow
+	
+func _process(delta):
+	global_position = following.global_position + (following.global_transform.y * follow_distance)
+
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "global_rotation", following.global_rotation, snap_duration)
--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+@export_group("Old")
 @export var speed_increase = 200
 @export var turn_speed = 5.0
 @export var max_velocity = 200
 @export var gravity = 450.8
 
+@export_group("Movement")
 @export var speed = 150
 @export var acceleration = 500
 @export var rotation_speed = 20.0
@@ -13,9 +15,26 @@ extends Node2D
 @export var max_speed = 2000
 @export var air_velocity = Vector2()
 
+@export_group("Body")
+@export var body_scene : PackedScene
+@export var body_segments =  5
+@export var scale_curve : Curve
+@export var body_scale = 2.0
+@export var snap_duration = 0.2
+@export var follow_distance = 30
+
 var is_in_bubble = true
 var velocity = Vector2()
 
+func _ready():
+	var last_actor = self
+	for i in body_segments:
+		var new_body = body_scene.instantiate() as Body
+		new_body.set_following(last_actor, snap_duration, follow_distance)
+		var scale_ratio =  float(i) / float(body_segments)
+		new_body.scale = new_body.scale * body_scale * scale_curve.sample(scale_ratio)
+		last_actor = new_body
+		add_sibling.call_deferred(new_body)
 	
 func _process(delta):
 	slither_movement(delta)
@@ -24,11 +43,11 @@ func is_boosting():
 	return Input.is_action_pressed("boost")
 	
 func slither_movement(delta):
-	if is_in_bubble:
-		# Rotation
-		var desired_rotation = global_position.angle_to_point(get_global_mouse_position()) + PI/2.0
-		global_rotation = rotate_toward(global_rotation, desired_rotation, delta * rotation_speed)
+	# Rotation
+	var desired_rotation = global_position.angle_to_point(get_global_mouse_position()) + PI/2.0
+	global_rotation = rotate_toward(global_rotation, desired_rotation, delta * rotation_speed)
 		
+	if is_in_bubble:
 		# Apply Friction and Acceleration
 		if is_boosting():
 			speed += acceleration * delta
@@ -43,7 +62,7 @@ func slither_movement(delta):
 		
 		$Label.text = str(speed)
 	else:
-		global_rotation = air_velocity.normalized().angle() + 90
+		#global_rotation = air_velocity.normalized().angle() + 90
 		air_velocity.y += gravity * delta
 		global_position += air_velocity * delta
 
@@ -69,7 +88,9 @@ func velocity_movement(delta):
 
 
 func _on_area_2d_area_entered(area):
-	is_in_bubble = true
+	if area.is_in_group("bubble"):
+		is_in_bubble = true
 
 func _on_area_2d_area_exited(area):
-	is_in_bubble = false
+	if area.is_in_group("bubble"):
+		is_in_bubble = false
