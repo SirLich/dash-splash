25867ea95adcb966f8bcff492a13f19e04c1005c
SirLich <lrkoehler77@gmail.com>
2025-01-26 15:02:48 +0100

    Add audio

--- actors/respawn_manager/respawn_manager.gd
+++ actors/respawn_manager/respawn_manager.gd
@@ -6,17 +6,22 @@ func _ready():
 	
 func on_player_fell(player : Player):
 	await get_tree().create_timer(1.0).timeout
+	$AudioStreamPlayer.play()
 	player.global_position = get_active_anchor().global_position
 	player.velocity = Vector2(0.0, 0.0)
 	
 func get_active_anchor():
 	for child in get_children():
+		if child is AudioStreamPlayer:
+			continue
 		if child.is_active:
 			return child
 	return null
 	
 func set_anchor_active(anchor: RespawnAnchor):
 	for child in get_children():
+		if child is AudioStreamPlayer:
+			continue
 		if child == anchor:
 			child.set_active(true)
 		else:
