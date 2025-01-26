8d673a963c031087f1f48d0f8eda464f0c0f9bd0
SirLich <lrkoehler77@gmail.com>
2025-01-26 12:50:05 +0100

    Add bus

--- actors/bus.gd
+++ actors/bus.gd
@@ -1,3 +1,17 @@
 extends Node
 
+
+var _color = Color.SEA_GREEN
+var _music_volume = 100.0
+var _sound_volume = 100.0
+
+func set_music_volume(volume: float):
+	_music_volume = volume
+	
+func set_sound_volume(volume: float):
+	_sound_volume = volume
+	
+func set_color(color : Color):
+	_color = color
+
 signal on_player_fell(player : Player)
